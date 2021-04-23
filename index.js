261ead5b7f878b29dee37df9a4522cfdbf9dd9d4
Charaf Mrah <charaf4charaf@gmail.com>
2021-04-23 02:26:21 +0000

    Added some test code to index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function NBAPlayer(name, team, position) {
+  this.name = name;
+  this.team = team;
+  this.position = position;
+}
+
+let curry = new NBAPlayer("Steph Curry", "GS Warriors", "Point Guard");
+
+curry;
+
